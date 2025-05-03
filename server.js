0d06f09a2232a79c444d32c2243290aec2becfea
TDH2KK1 <dinhhientran9998@gmail.com>
2025-05-03 13:03:10 +0700

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],  
+    origin: [process.env.FE_CLIENT_URL, process.env.FE_ADMIN_URL],  
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'token']
